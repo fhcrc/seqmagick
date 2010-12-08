0664aa5d7b09f0f25de40e0c81ac29f6cfa6a3b3
Brian Hodges <bhodges@fhcrc.org>
2010-12-08 10:02:48 -0800

    added ignore for build dir

--- src/SeqMagick/build/lib.linux-x86_64-2.6/SeqMagick/t.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-from magickwrap import MagickWrap
-
-magick = MagickWrap(in_file='examples/aligned.fasta', out_file="test.sth")
-
-magick.convert_format()
-
-#print magick.source_file_type
