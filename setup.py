386c72ee6493bf670b0164d9f605d04d8934c463
Connor McCoy <cmccoy@fhcrc.org>
2011-04-07 11:10:08 -0700

    Restructure package
    * remove src/ directory
    * Move CONTRIB, INSTALL, LICENCE, CHANGELOG to top level
    * move seqmagick script to seqmagick/scripts

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name = 'seqmagick',
       version = '0.1',
       description = 'Tools for converting and working with sequence files.',
       author = 'Erick Matsen <ematsen@gmail.com>, Brian Hodges <bhodges@fhcrc.org>',
-      package_dir = {'seqmagick': '.'},
-      packages = ['seqmagick'],
+      packages = ['seqmagick', 'seqmagick.scripts'],
       scripts = ['scripts/seqmagick'],
       )
