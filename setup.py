2675ca22e12980517939b222a69e43b1d086a40a
Connor McCoy <connorm@gmail.com>
2011-05-10 21:40:15 -0700

    Add a space for unit tests
    So far: squeeze, pattern_replace, based on what I did most recently, and
    what I want to change slightly.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name = 'seqmagick',
       version = '0.1',
       description = 'Tools for converting and working with sequence files.',
       author = 'Erick Matsen <ematsen@gmail.com>, Brian Hodges <bhodges@fhcrc.org>',
-      packages = ['seqmagick', 'seqmagick.scripts'],
+      packages = ['seqmagick', 'seqmagick.scripts', 'seqmagick.test'],
       scripts = ['scripts/seqmagick'],
       )
