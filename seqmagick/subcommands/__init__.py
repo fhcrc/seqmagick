d69693f3fd4c6d41bc00f7edf1f75b5767ab5db8
Connor McCoy <cmccoy@fhcrc.org>
2011-05-18 12:40:33 -0700

    Replace '_' with '-' in subcommands

--- seqmagick/subcommands/__init__.py
+++ seqmagick/subcommands/__init__.py
@@ -2,4 +2,5 @@ commands = 'convert', 'info', 'mogrify', 'muscle', 'primer_trim'
 
 def itermodules(root=__name__):
     for command in commands:
-        yield command, __import__('%s.%s' % (root, command), fromlist=[command])
+        yield (command.replace('_', '-'),
+               __import__('%s.%s' % (root, command), fromlist=[command]))
