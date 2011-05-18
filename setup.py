623324dbf62d568b7b91d316f713e54ae58972ce
Connor McCoy <cmccoy@fhcrc.org>
2011-05-18 16:11:16 -0700

    Update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
 #! /usr/bin/env python
 
 from distutils.core import setup
+import sys
 
 from seqmagick import __version__ as version
 
+if sys.version_info < (2, 7):
+    print 'ERROR: seqmagick requires at least Python 2.7 to run.'
+    sys.exit(1)
+
+try:
+    import Bio
+except:
+    print "ERROR: BioPython isn't installed."
+    sys.exit(1)
+
 setup(name='seqmagick',
       version=version,
-      description='Tools for converting and working with sequence files.',
-      author='Erick Matsen <ematsen@gmail.com>, Brian Hodges <bhodges@fhcrc.org>',
+      description='Tools for converting and modifying sequence files '
+                  'from the command-line',
+      url='http://github.com/fhcrc/seqmagick',
+      author='Erick Matsen et al',
+      author_email="matsen@fhcrc.org",
       packages=['seqmagick', 'seqmagick.scripts', 'seqmagick.test',
                 'seqmagick.subcommands'],
       scripts=['scripts/seqmagick'],
       requires=['Python (>= 2.7)', 'biopython (>=1.54)'],
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+          ],
+      license="GPL V3",
       )
