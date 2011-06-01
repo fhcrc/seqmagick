12542061bfb476c05ed53de3c1b810e117f44474
Connor McCoy <cmccoy@fhcrc.org>
2011-06-01 14:48:58 -0700

    Infer output format from extension
    Clean up some documentation, as well

--- seqmagick/subcommands/quality_filter.py
+++ seqmagick/subcommands/quality_filter.py
@@ -5,9 +5,12 @@ Filter reads based on quality scores
 import argparse
 import collections
 import itertools
+import os.path
 
 from Bio import SeqIO
 from Bio.SeqIO import QualityIO
+
+from seqmagick import fileformat
 
 
 def build_parser(parser):
@@ -18,27 +21,35 @@ def build_parser(parser):
             help='Input fasta file')
     parser.add_argument('input_qual', type=argparse.FileType('r'),
             help='The quality scores associated with fasta_file')
-    parser.add_argument('output_fasta', type=argparse.FileType('w'),
-            help='Output FASTA')
-
-    parser.add_argument('--min-mean-quality', type=float,
-            default=25.0, help="""Minimum mean quality score for each read
+    parser.add_argument('output_file', type=argparse.FileType('w'),
+            help="""Output file. Format determined from extension.""")
+    parser.add_argument('--min-mean-quality', metavar='QUALITY', type=float,
+            default=25, help="""Minimum mean quality score for each read
             (default: %(default)s""")
-    parser.add_argument('--quality-window', type=int,
-            default=0, help="""Window size for truncating sequences once the
-            mean quality within the window drops below --min-mean-quality.
-            0=use whole sequence. (default: %(default)s)""")
+    parser.add_argument('--quality-window', type=int, metavar='WINDOW_SIZE',
+            default=0, help="""Window size for truncating sequences.  When set
+            to a non-zero value, sequences are truncated where the mean mean
+            quality within the window drops below --min-mean-quality.
+            (default: %(default)s)""")
 
     parser.add_argument('--ambiguous-action', choices=('truncate', 'drop'),
             help="""Action to take on ambiguous base in sequence (N's).
             Default: no action.""")
 
+
 def mean(sequence):
+    """
+    Calculates the arithmetic mean of a list / tuple
+    """
     return sum(sequence) / float(len(sequence))
 
-def moving_average(iterable, n=3):
-    # moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
-    # http://en.wikipedia.org/wiki/Moving_average
+
+def moving_average(iterable, n):
+    """
+    From Python collections module documentation
+
+    moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
+    """
     it = iter(iterable)
     d = collections.deque(itertools.islice(it, n-1))
     d.appendleft(0)
@@ -47,6 +58,7 @@ def moving_average(iterable, n=3):
         s += elem - d.popleft()
         d.append(elem)
         yield s / float(n)
+
 
 
 class QualityScoreFilter(object):
@@ -65,12 +77,15 @@ class QualityScoreFilter(object):
         Returns None
         """
         quality_scores = record.letter_annotations['phred_quality']
+
         # Simple case - no window size or window covers whole sequence
         if self.window_size == 0 or len(record) <= self.window_size:
             mean_score = mean(quality_scores)
             return record if mean_score >= self.min_mean_score else None
 
-        # Find the right clipping point
+        # Find the right clipping point. Start clipping at the beginning of the
+        # sequence, then extend the window to include regions with acceptable
+        # mean quality scores.
         clip_right = 0
         for i, a in enumerate(moving_average(quality_scores,
             self.window_size)):
@@ -114,8 +129,13 @@ def ambiguous_base_filter(records, action):
             assert False
 
 def action(arguments):
+    """
+    Given parsed arguments, filter input files.
+    """
     qfilter = QualityScoreFilter(arguments.min_mean_quality,
             arguments.quality_window)
+
+    output_type = fileformat.from_filename(arguments.output_fasta.name)
     with arguments.input_fasta:
         with arguments.input_qual:
             sequences = QualityIO.PairedFastaQualIterator(
@@ -127,6 +147,6 @@ def action(arguments):
 
             with arguments.output_fasta:
                 count = SeqIO.write(filtered, arguments.output_fasta,
-                        'fasta')
+                        output_type)
 
             print count
