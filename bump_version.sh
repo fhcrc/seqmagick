854d168be4e23f078343e03752e3534229e0dcba
Connor McCoy <connorm@gmail.com>
2011-07-12 08:28:38 -0700

    Versioning script

---
+++ bump_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -u
+
+NEW_VERSION=$1
+
+sed -i '' -e "s/__version__ =.*/__version__ = '$NEW_VERSION'/" seqmagick/__init__.py
+
+sed -i '' -e "s/release =.*/release = '$NEW_VERSION'/" docs/conf.py
