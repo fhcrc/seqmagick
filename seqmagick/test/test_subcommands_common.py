c60fe0ef2d3755e2aa4778cb2c98968f3559adf8
Connor McCoy <cmccoy@fhcrc.org>
2011-08-25 15:15:50 -0700

    Consolidate common tests

--- seqmagick/test/test_subcommands_common.py
+++ seqmagick/test/test_subcommands_common.py
@@ -41,3 +41,29 @@ class PartialAppendTestCase(unittest.TestCase):
         a(None, self.namespace, [0, 2])
         f = self.namespace.functions[0]
         self.assertEqual([0, 1], f(range(10)))
+
+class PositiveValueTestCase(unittest.TestCase):
+
+    def test_negative(self):
+        self.assertRaises(argparse.ArgumentTypeError,
+                common.positive_value(int), '-1')
+
+    def test_positive(self):
+        self.assertEqual(1, common.positive_value(int)('1'))
+
+    def test_zero(self):
+        self.assertEqual(0, common.positive_value(int)('0'))
+
+class CutRangeTestCase(unittest.TestCase):
+    def test_negative(self):
+        self.assertRaises(argparse.ArgumentTypeError,
+                common.cut_range, '0:5')
+
+    def test_out_of_order(self):
+        self.assertRaises(argparse.ArgumentTypeError,
+                common.cut_range, '10:5')
+
+    def test_start(self):
+        actual = common.cut_range('5:10')
+        self.assertEqual(4, actual.start)
+        self.assertEqual(10, actual.stop)
