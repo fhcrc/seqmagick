4915705a9f93fe7b61af21db13d1554a63af107c
Connor McCoy <cmccoy@fhcrc.org>
2013-01-30 12:27:30 -0800

    Bump version for dev

--- bump_version.sh
+++ bump_version.sh
@@ -5,6 +5,5 @@ set -u
 
 NEW_VERSION=$1
 
-sed -i '' -e "s/__version__ =.*/__version__ = '$NEW_VERSION'/" seqmagick/__init__.py
-
-sed -i '' -e "s/release =.*/release = '$NEW_VERSION'/" docs/conf.py
+sed -i -e 's/__version__ =.*/__version__ = '"'$NEW_VERSION'"'/' seqmagick/__init__.py docs/conf.py
+sed -i -e "s/release =.*/release = '$NEW_VERSION'/" docs/conf.py
