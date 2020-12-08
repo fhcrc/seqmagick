81b47391357ca383cec6d0ed2498b47ad04e0fe8
jgallowa07 <jaredgalloway07@gmail.com>
2020-12-08 09:34:53 -0800

    updated requirement for pygtrie

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='seqmagick',
       setup_requires=['nose>=1.0'],
       python_requires='>=3.5',
       test_suite='nose.collector',
-      install_requires=['biopython>=1.78', 'pygtrie>=2.4.1'],
+      install_requires=['biopython>=1.78', 'pygtrie>=2.1'],
       classifiers=[
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Development Status :: 4 - Beta',
